aaf1a651baeae1bf822fa4b5bca57383017c9a1d
TUNAHAN KAYA <tunahankaya1@hotmail.com>
2021-01-23 15:17:57 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string,int> sozluk=new MyDictionary<string,int>();
+            sozluk.Add("l",3);
+            sozluk.Add("a", 5);
+            sozluk.Add("b", 6);
+            sozluk.List();
+        }
+    }
+
+    class MyDictionary<Tkey,Tvalue>
+    {
+         Tkey[] _key;
+        Tvalue[] _value;
+         
+
+        public MyDictionary()
+        {
+            _key=new Tkey[0];
+            _value=new Tvalue[0];
+
+        }
+
+        public void Add(Tkey item ,Tvalue item2)
+        {
+            Tkey[] tempt = _key;
+            _key=new Tkey[_key.Length+1];
+            for (int i = 0; i < tempt.Length; i++)
+            {
+                _key[i] = tempt[i];
+            }
+
+            _key[_key.Length - 1] = item;
+
+            Tvalue[] tempt2 = _value;
+            _value=new Tvalue[_value.Length+1];
+            for (int i = 0; i < tempt2.Length; i++)
+            {
+                _value[i] = tempt2[i];
+            }
+
+            _value[_value.Length - 1] = item2;
+
+        }
+
+        public void List()
+        {
+
+            for (int i = 0; i < _key.Length; i++)
+            {
+                Console.WriteLine(_key[i] +","+ _value[i]);
+            }
+        }
+    }
+}
---
+++ denemeee/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace denemeee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> sehirler=new List<string>();
+            sehirler.Add("ist");
+            int count = sehirler.Count;
+            Console.WriteLine(count);
+            foreach (var x in sehirler)
+            {
+                Console.WriteLine(x);
+            }
+
+            MyList<string> sehirler2=new MyList<string>();
+            sehirler2.Add("İzm");
+            sehirler2.List();
+        }
+    }
+
+
+
+   public class MyList<T>
+   { 
+       T[] sehirler2;
+        T[] _array;
+
+       public MyList()
+       {
+           sehirler2=new T[0];
+       }
+
+       public void Add(T item )
+       {
+           
+           _array = sehirler2;
+           sehirler2=new T[sehirler2.Length+1];
+           for (int i = 0; i < _array.Length; i++)
+           {
+               sehirler2[i] = _array[i];
+           }
+
+           sehirler2[sehirler2.Length - 1] = item;
+       }
+
+       public void List()
+       {
+           foreach (var y in sehirler2)
+           {
+               Console.WriteLine(y);
+           }
+       }
+
+
+   }
+}
